e60e510f83b26e6546c03708728d7d99369d920f
talbaror <tbaror@gmail.com>
2019-06-30 21:43:03 +0300

    add classes to registration form

--- mysite/authenticate/forms.py
+++ mysite/authenticate/forms.py
@@ -6,11 +6,18 @@ class SignUpForm(UserCreationForm):
     """SignUp UserCreationForm."""
 
     # TODO: Define form fields he
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model = User
         fields = ('username','first_name', 'last_name', 'email', 'password1', 'password2',)
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)    
+       
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
 
 
