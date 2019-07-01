fb9d0b26c5d466c425fed333e416bbe0e59eb704
talbaror <tbaror@gmail.com>
2019-07-01 22:24:36 +0300

    label help text

--- mysite/authenticate/forms.py
+++ mysite/authenticate/forms.py
@@ -6,9 +6,9 @@ class SignUpForm(UserCreationForm):
     """SignUp UserCreationForm."""
 
     # TODO: Define form fields he
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    first_name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control'}))
-    last_name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Email Address'}))
+    first_name = forms.CharField(label='', max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
+    last_name = forms.CharField(label='', max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Lastname'}))
     class Meta:
         model = User
         fields = ('username','first_name', 'last_name', 'email', 'password1', 'password2',)
@@ -17,7 +17,15 @@ class SignUpForm(UserCreationForm):
         super(SignUpForm, self).__init__(*args, **kwargs)    
        
         self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['username'].label= ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] ='Enter Password'
+        self.fields['password1'].label=''
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label=''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small> Enter the same password as before, for verification.</small></span>'
 
 
